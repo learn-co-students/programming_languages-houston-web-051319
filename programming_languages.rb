f1dc0d847e42fc6751436033e8d8a7afc30e3546
aruande <kalunga212@gmail.com>
2019-04-29 15:17:28 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type|
+    type[1].each do |style|
+    if new_hash[style[0]]== nil
+      new_hash[style[0]] = style [1]
+    end
+    if new_hash[style[0]][:style] == nil
+      new_hash[style[0]][:style] = [type[0]]
+    elsif !new_hash[style[0]][:style].include?(type[0])
+      new_hash[style[0]][:style] << type[0]
+    end
+  end
+    
+  end 
+  new_hash    
 end
